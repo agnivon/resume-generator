290f510f499f932170d61a1dda3fd61af1740187
agnivon <agnivon@gmail.com>
2023-11-17 00:14:11 +0530

    fix: removed getProviders

--- src/components/pages/SignInPage.tsx
+++ src/components/pages/SignInPage.tsx
@@ -1,16 +1,15 @@
-import { getProviders } from "next-auth/react";
-import SignInCard from "../feature/auth/SignInCard";
+import { Routes } from "@/constants/routes.constants";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { Routes } from "@/constants/routes.constants";
+import SignInCard from "../feature/auth/SignInCard";
 
 export default async function SignInPage() {
   const session = await getServerSession();
   if (session) redirect(Routes.HOME);
-  const providers = await getProviders();
+  //const providers = await getProviders();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <SignInCard providers={providers} />
+      <SignInCard /* providers={providers} */ />
     </div>
   );
 }
