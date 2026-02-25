6e5cd192e08310fc313aa98bd1e6d3a31dad44a5
agnivon <agnivon@gmail.com>
2026-02-25 21:55:32 +0530

    fix: react-alert Provider

--- src/context/global/AppProvider.tsx
+++ src/context/global/AppProvider.tsx
@@ -1,26 +1,25 @@
-import { getServerSession } from "next-auth";
-import AlertProvider from "./AlertProvider";
-import ReduxProvider from "./ReduxProvider";
-import SessionProvider from "./SessionProvider";
-import ThemeContextProvider from "./ThemeContextProvider";
-import ReactQueryProvider from "./ReactQueryProvider";
 import { getNextAuthServerSession } from "@/utils/session.utils";
+import SessionProvider from "./SessionProvider";
+import ReduxProvider from "./ReduxProvider";
+import ReactQueryProvider from "./ReactQueryProvider";
+import ThemeContextProvider from "./ThemeContextProvider";
+import AlertProvider from "./AlertProvider";
 
 const AppProvider = async ({ children }: { children: React.ReactNode }) => {
   const session = await getNextAuthServerSession();
 
   return (
-    <>
-      <SessionProvider session={session}>
-        <ReduxProvider>
-          <ReactQueryProvider>
-            <ThemeContextProvider>
-              <AlertProvider>{children}</AlertProvider>
-            </ThemeContextProvider>
-          </ReactQueryProvider>
-        </ReduxProvider>
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <ReduxProvider>
+        <ReactQueryProvider>
+          <ThemeContextProvider>
+            <AlertProvider>
+              {children}
+            </AlertProvider>
+          </ThemeContextProvider>
+        </ReactQueryProvider>
+      </ReduxProvider>
+    </SessionProvider>
   );
 };
 
